31f96451882c38e98985a3b0b7dbc362964ddf88
nyteghost <nytemmo@gmail.com>
2022-09-30 19:48:03 -0400

    Update createStaffTopBlockTicket.py

--- createStaffTopBlockTicket.py
+++ createStaffTopBlockTicket.py
@@ -21,7 +21,7 @@ conn = sa.create_engine("mssql+pyodbc:///?odbc_connect={}".format(params))
 
 
 ### SQL Query
-staffTopsBlockquery = f"exec GCAAssetMGMT_2_0.Rep.uspNewStaffTerms"
+staffTopsBlockquery = f"exec SomeSchoolAssetMGMT_2_0.Rep.uspNewStaffTerms"
 staffTopsBlock = pd.read_sql(staffTopsBlockquery , conn)
 
 ### Create list of staff members that are being disabled
@@ -32,7 +32,7 @@ list_a= "\n".join(list_a)
 
 ### Creation of ticket
 company_id = 22482 #the id of the company
-board_name = "GCA Service" #the id of the board
+board_name = "SomeSchool Service" #the id of the board
 board_id = 29
 summary = 'This is a test of the API broadcast system'
 resources = 'MBrown'  #should match a cw username
@@ -40,7 +40,7 @@ url = 'https://api-na.myconnectwise.net/v2021_3/apis/3.0/'
 auth = config["cwAUTH"]
 newTicket = ticket.Ticket(json_dict={
         "company": {"id": "22482"},
-        "board": {"name": "GCA Service"},
+        "board": {"name": "SomeSchool Service"},
         "summary":summary,
                 "resources": resources})
 
@@ -52,7 +52,7 @@ print(tID)
 ### Assigns ticket
 itID = int(tID)
 assign_ticket = schedule_entries_api.ScheduleEntriesAPI(url=url, auth=auth)
-assigned = schedule_entry.ScheduleEntry({"objectId": itID, "member":{"identifier":"AMaag"},"type": { "identifier": "S" },"ownerFlag": True})
+assigned = schedule_entry.ScheduleEntry({"objectId": itID, "member":{"identifier":"APerson"},"type": { "identifier": "S" },"ownerFlag": True})
 assign_ticket.create_schedule_entry(assigned)
 
 ### Creat a ticket note
@@ -70,7 +70,7 @@ ticket_notes.create_ticket_note(note)
 #     "id": "22482"
 #   },
 #   "board": {
-#     "name": "GCA Service"
+#     "name": "SomeSchool Service"
 #   },
 #   "resources": "MBrown"
 # })
